1fa9dd93bb7c8321393931553eaa80f9e3aa4341
lukethun <lukethun@optusnet.com.au>
2024-01-09 09:56:48 +1000

    Add buttons to index.html and begin JavaScript functions

---
+++ javascript.js
@@ -0,0 +1,24 @@
+function add(a, b){
+    return a + b;
+}
+
+function subtract(a, b){
+    return a - b;
+}
+
+function multiply(a, b){
+    return a * b;
+}
+
+function divide(a, b){
+    return a / b;
+}
+
+
+let firstNumber = 0;
+let secondNumber = 0;
+let operator = 0;
+
+function operate(operator, num1, num2){
+    
+}
