0cf31a62e6becb20aaea9811f32e962ae3ca9558
lukethun <lukethun@optusnet.com.au>
2024-01-09 12:06:58 +1000

    Forgot to commit earlier... Add CSS styling for calculator. Add JS to make display and operators function.

--- javascript.js
+++ javascript.js
@@ -17,8 +17,84 @@ function divide(a, b){
 
 let firstNumber = 0;
 let secondNumber = 0;
-let operator = 0;
+let op = '';
+let displayValue = '';
 
-function operate(operator, num1, num2){
-    
+const numbers = document.querySelectorAll('.number');
+const display = document.querySelector('.display');
+const clear = document.querySelector('.clear');
+const dlt = document.querySelector('.delete');
+const operators = document.querySelectorAll('.operator');
+const eql = document.querySelector('#equals');
+
+
+// Update display when a number is pressed
+numbers.forEach(number => {
+    number.addEventListener('click', () => {
+        displayValue += number.textContent;
+        display.textContent = displayValue;
+
+        if(displayValue.length === 25){
+            displayValue = displayValue.slice(0, -1);
+            display.textContent = displayValue;
+        }
+    })
+})
+
+// Clear display when clear-button is clicked
+clear.addEventListener('click', () => {
+    clearDisplay();
+})
+
+// Remove last digit of display
+dlt.addEventListener('click', () => {
+    displayValue = displayValue.slice(0, -1);
+    display.textContent = displayValue;
+    displayLengthCheck();
+})
+
+operators.forEach(operator => {
+    operator.addEventListener('click', () => {
+        firstNumber = displayValue;
+        clearDisplay();
+        op = operator.textContent
+    })
+})
+
+eql.addEventListener('click', () => {
+    secondNumber = displayValue;
+    clearDisplay();
+    operate(firstNumber, secondNumber, op);
+})
+
+function displayLengthCheck(){
+    if(displayValue.length === 0){
+        display.textContent = 0;
+    }
+}
+
+function clearDisplay(){
+    displayValue = '';
+    display.textContent = 0;
+}
+
+function operate(num1, num2, operator){
+    num1 = Number(num1);
+    num2 = Number(num2);
+    if(operator === '+'){
+        displayValue = add(num1, num2);
+        display.textContent = displayValue;
+    }
+    if(operator === '-'){
+        displayValue = subtract(num1, num2);
+        display.textContent = displayValue;
+    }
+    if(operator === '×'){
+        displayValue = multiply(num1, num2);
+        display.textContent = displayValue; 
+    }
+    if(operator === '÷'){
+        displayValue = divide(num1, num2);
+        display.textContent = displayValue; 
+    }
 }
